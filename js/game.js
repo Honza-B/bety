8dcff95281405ee3a3c7fb86c3ca55a6e87078bf
honza_b <benedikj.jan@gmail.com>
2015-06-16 23:52:52 +0200

    kiwi.js and background add

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var game = new Kiwi.Game();
+var state = new Kiwi.State( 'statovni' );
+
+state.preload = function() {
+    Kiwi.State.prototype.preload.call(this);
+
+    this.addImage( 'background', 'assets/bg.png' );
+}
+
+state.create = function() {
+    Kiwi.State.prototype.create.call( this );
+
+    this.background = new Kiwi.GameObjects.StaticImage( this, this.textures.background, 0, 0 );
+}
