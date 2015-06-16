73c3f632b75edd352cbeecd35431e619a47dbfd3
honza_b <benedikj.jan@gmail.com>
2015-06-16 23:55:33 +0200

    ios target

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var game = new Kiwi.Game();
+var game = new Kiwi.Game( null, 'myGame' , null , { deviceTarget: Kiwi.TARGET_COCOON } );
 var state = new Kiwi.State( 'statovni' );
 
 state.preload = function() {
