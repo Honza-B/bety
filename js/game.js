a5e80d06ed49559f34881ea5f5159023f0fbae98
honza-b <benedikj.jan@gmail.com>
2015-06-17 19:23:23 +0200

    state switch

--- js/game.js
+++ js/game.js
@@ -44,3 +44,36 @@ state.update = function(){
 state.buttonPressed = function() {
     game.states.switchState( nextstage );
 }
+
+nextstage.preload = function() {
+    Kiwi.State.prototype.preload.call(this);
+
+    this.addImage( 'background-next', 'assets/bg.png' );
+}
+
+nextstage.create = function() {
+    Kiwi.State.prototype.create.call( this );
+
+    this.myButton = new Kiwi.HUD.Widget.Button( this.game, 'PRESS ME!', 50, 50 );
+    this.game.huds.defaultHUD.addWidget( this.myButton );
+
+    this.myButton.style.color = 'white';
+    this.myButton.style.fontSize = '2em';
+    this.myButton.style.fontWeight = 'bold';
+    this.myButton.style.padding = '0.5em 1em';
+    this.myButton.style.backgroundColor = 'black';
+    this.myButton.style.cursor = 'pointer';
+
+    this.myButton.input.onDown.add( this.buttonPressed, this );
+
+    this.background = new Kiwi.GameObjects.StaticImage( this, this.textures.background, 0, 0 );
+    this.addChild(this.background);
+}
+
+nextstage.update = function(){
+    Kiwi.State.prototype.update.call(this);
+}
+
+nextstage.buttonPressed = function() {
+    game.states.switchState( state );
+}
