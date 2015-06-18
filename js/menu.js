0b6aac36635c8bc1675c1cadf06936eb8d98345e
honza_b <benedikj.jan@gmail.com>
2015-06-18 19:30:05 +0200

    kids page added

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,30 @@
-$('.start-btn-next').click(function() {
+$('.start-btn-next, .kids-btn-prev').click(function() {
     $('.parents-page').css('display','block');
 });
 
 $('.parents-btn-prev').click(function() {
     $('.parents-page').css('display','none');
+});
+
+$('.parents-btn-next').click(function() {
+    $('.kids-page').css('display','block');
+});
+
+$('.kids-btn-prev').click(function() {
+    $('.kids-page').css('display','none');
+});
+
+$('.kid-add-name').click(function() {
+    $(this).css({
+        'width' : '195px',
+        'background-position' : '169px 50%',
+        'padding-left' : '54px'
+    });
+    $('.kid-add-title').css({
+        'width' : '0',
+        'height' : '0'
+    });
+    $('.kids-age').css({
+        'height' : '32px'
+    });
 });
