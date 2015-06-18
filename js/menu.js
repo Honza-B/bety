277d7c75ca5d93d547a7984f8dfb42e397dcd015
honza_b <benedikj.jan@gmail.com>
2015-06-18 18:21:00 +0200

    parents page adde

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+$('.start-btn-next').click(function() {
+    $('.parents-page').css('display','block');
+});
+
+$('.parents-btn-prev').click(function() {
+    $('.parents-page').css('display','none');
+});
